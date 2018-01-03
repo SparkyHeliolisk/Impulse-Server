20a5c929683242725d9c9dac6f8e6d1ebf660b6a
SparkyHeliolisk <SparkyHeliolisk@users.noreply.github.com>
2018-01-03 10:24:19 +0000

    Update credits.js

--- wavelength-plugins/credits.js
+++ wavelength-plugins/credits.js
@@ -17,7 +17,7 @@ exports.commands = {
 			 "- " + WL.nameColor('Xirizu', true) + " (Community Admin, Aura Storm)<br />" +
 			 "- " + WL.nameColor('SnorlaxTheRain', true) + " (Community Admin)<br />" +
 			 "<br />" +
-			 "<u><b>Base Repositories:</b></u><br />"
+			 "<u><b>Base Repositories:</b></u><br />" +
 			 "- " + WL.nameColor('HoeenHero', true) + " (Wavelength)<br />" +
 			 "- " + WL.nameColor('Insist', true) + " (Exiled)<br />" +
 			 //"<u><b>Retired Staff:</b></u><br />"
